2b0fda232f86ff316091a6a5785ffef9fd034583
hfaran <hamzafaran@outlook.com>
2015-10-15 20:56:54 -0700

    Working server and clinet

--- ect/message.py
+++ ect/message.py
@@ -1,6 +1,65 @@
 import socket
+from abc import abstractproperty
+from abc import abstractmethod
 
 
+class Sock(object):
+    """Base class for Client and Server"""
 
-class Client(object):
-    def __init__(self, port):
+    BUFSIZE = 4096
+
+    @abstractproperty
+    def conn(self):
+        raise NotImplementedError
+
+    def _encrypt(self, msg):
+        # TODO
+        return msg
+
+    def _decrypt(self, msg):
+        # TODO
+        return msg
+
+    def send(self, msg):
+        self.conn.send(self._encrypt(msg))
+
+    def recv(self):
+        return self._decrypt(self.conn.recv(self.BUFSIZE))
+
+    @abstractmethod
+    def close(self):
+        raise NotImplementedError
+
+
+class Client(Sock):
+    """Client
+
+    :param str remote_ip: Remote IP
+    :param int remote_port: Remote Port
+    """
+    def __init__(self, remote_ip, remote_port):
+        self._sock = socket.socket()
+        self._sock.connect((remote_ip, remote_port))
+
+    @property
+    def conn(self):
+        return self._sock
+
+    def close(self):
+        self._sock.close()
+
+
+class Server(Sock):
+    def __init__(self, ip, port):
+        self.sock = socket.socket()
+        self.sock.bind((ip, port))
+        self.sock.listen(5)
+        self._conn, (self.client_ip, self.client_port) = self.sock.accept()
+
+    @property
+    def conn(self):
+        return self._conn
+
+    def close(self):
+        self.conn.close()
+        self.sock.close()

15be6859c076f1d51351798b00b2b823ed98fd77
Dylan Dumesnil <dadumesnil@gmail.com>
2015-10-15 19:26:54 -0700

    GUI started, going with tabbed layout

--- gui.py
+++ gui.py
@@ -1,1 +1,20 @@
-__author__ = 'hfaran'
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.config import Config
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.floatlayout import FloatLayout
+
+
+class GuiWidget(TabbedPanel):
+    pass
+
+
+class GuiApp(App):
+    def build(self):
+        return GuiWidget()
+
+
+if __name__ == '__main__':
+    Config.set('graphics', 'width', '400')
+    Config.set('graphics', 'height', '600')
+    GuiApp().run()

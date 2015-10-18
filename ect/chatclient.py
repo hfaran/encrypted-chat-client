3e22aea83db713f6663c7b593405c7c5da21718b
whiteSkar <sypower07@hotmail.com>
2015-10-17 17:33:20 -0700

    basic mutual auth with key exchange using dummy values

--- ect/chatclient.py
+++ ect/chatclient.py
@@ -4,6 +4,8 @@ from abc import abstractmethod
 from ect.message import Client
 from ect.message import Server
 
+import ect.crypto
+
 
 class ChatClientBase(object):
 
@@ -38,6 +40,10 @@ class ChatClientClient(ChatClientBase):
         self.server = Server(local_ip, local_port)
         self._kes = self.KEY_EXCHANGE_STATES[-1]  # key exchange state
         self._session_key = None
+        self._shared_key = None
+        self._secret_value = 1357 # TODO
+        self._g = 3 # TODO
+        self._p = 35791 #TODO
 
     KEY_EXCHANGE_STATES = {
         -1: None,
@@ -50,15 +56,20 @@ class ChatClientClient(ChatClientBase):
     def Ks(self):
         return self._session_key
 
+    # GUI should call this fucntion to set the shared key set by TA
+    def set_shared_key(self, key=None):
+        self._shared_key = key
+    
     def mutauth_step(self, reset=False):
-        """This method steps through mutual authentication using Diffie-Helman
+        """This method steps through mutual authentication 
+            and key exchange using Diffie-Helman
 
         Each call to this method will perform one step out of the
-         total steps necessary for authentication.
+         total steps necessary for authenticationr
 
         :param bool reset: If this is set, we reset back to step -1
         """
-        if reset:
+        if reset or self._shared_key is None:
             self._kes = self.KEY_EXCHANGE_STATES[-1]
             return
 
@@ -74,11 +85,27 @@ class ChatClientClient(ChatClientBase):
             # ...
             # Once verified, we can assign the session key and consider
             #  ourselves authenticated
-            self._session_key = "something to do with {}".format(resp)
+
+            self._rb = resp[:crypto.BLOCK_SIZE]
+            ct = resp[crypto.BLOCK_SIZE:]
+            pt = crypto.decrypt(self._shared_key, ct)
+            # what's the size of identifier, ra, and gb mod p? are they all fixed
+            identifier, ra, gb_mod_p = pt[:crypto.BLOCK_SIZE],
+                                       pt[crypto.BLOCK_SIZE:crypto.BLOCK_SIZE],
+                                       pt[-crypto.BLOCK_SIZE:]
+            if (self._Ra != ra)
+                pass # TODO: do something it's Trudy
+
+            # TODO check if correct
+            self._session_key = pow(gb_mod_p, self._secret_value) % self._p
             self._kes = self.KEY_EXCHANGE_STATES[1]
         elif self._kes == self.KEY_EXCHANGE_STATES[1]:
             # Send E("Alice", RB, ga mod p, KAB)
-            self.client.send("something_else")
+            identifier = os.urandom(crypto.BLOCK_SIZE)
+            ga_mod_p = pow(self._g, self._secret_value) % self._p
+            pt = identifier + self._rb + ga_mod_p
+            ct = crypto.encrypt(self._shared_key, pt) 
+            self.client.send(ct)
             self._kes = self.KEY_EXCHANGE_STATES[2]
         elif self._kes == self.KEY_EXCHANGE_STATES[2]:
             raise StopIteration("Authentication completed successfully.")

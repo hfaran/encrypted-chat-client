8240e482711b51f3c40cc97302cfa5380432a766
whiteSkar <sypower07@hotmail.com>
2015-10-21 20:25:54 -0700

    add some logging comment and raise exception when shared key is not set

--- ect/chatclient.py
+++ ect/chatclient.py
@@ -8,6 +8,7 @@ from abc import abstractmethod
 from ect import crypto
 from ect.exceptions import BeingAttacked
 from ect.exceptions import NoAuthentication
+from ect.exceptions import NoSharedKey
 from ect.message import Client
 from ect.message import Server
 from ect.crypto import derive_new_key
@@ -159,32 +160,36 @@ class ChatClientClient(ChatClientBase):
 
         :param bool reset: If this is set, we reset back to step -1
         """
-        if reset or self._shared_key is None:
+        if reset:
             self._mutau_state = self.MUTUAL_AUTH_STATES[-1]
-            # TODO: GUI should show something when shared key is not set
             return
+
+        if self._shared_key is None:
+            raise NoSharedKey("Shared key is not setup yet. Can't proceed")
 
         if self._mutau_state == self.MUTUAL_AUTH_STATES[-1]:
             # Send our public key (Ra)
             self._Ra = os.urandom(crypto.BLOCK_SIZE)
             self.client.send(self._Ra)
-            
+            # log: Client sends Ra
+
             self._mutau_state = self.MUTUAL_AUTH_STATES[0]
         elif self._mutau_state == self.MUTUAL_AUTH_STATES[0]:
             # Get response: RB, E("Bob", RA, gb mod p, KAB)
             resp = self.server.recv()
             self._rb = resp[:crypto.BLOCK_SIZE]
+            # log: client receives rb
             ct = resp[crypto.BLOCK_SIZE:]
             pt = crypto.decrypt(self._shared_key, ct)
             self._server_ident, ra, gb_mod_p = self.extract_auth_msg_parts(pt)
+            # log: client receives identifier of server, ra, and gb mod p
 
-            # TODO: probably GUI should call this function with reset=True
-            # and display being attacked
             if ra != self._Ra:
                 raise BeingAttacked("Trudy is attacking")
 
             self._session_key = pow(long(gb_mod_p), self._secret_value, self._p)
-            
+            # log: client creates the session key
+
             self._mutau_state = self.MUTUAL_AUTH_STATES[1]
         elif self._mutau_state == self.MUTUAL_AUTH_STATES[1]:
             # Send E("Alice", RB, ga mod p, KAB)
@@ -193,9 +198,14 @@ class ChatClientClient(ChatClientBase):
             identifier = os.urandom(crypto.BLOCK_SIZE)
             while identifier == self._server_ident:
                 identifier = os.urandom(crypto.BLOCK_SIZE)
-            
+            # log: client creates its identifier that's not the same as server's
+ 
             ga_mod_p = pow(self._g, self._secret_value, self._p)
+            # log: client creates ga mod p
+ 
             pt = identifier + self._rb + str(ga_mod_p)
+            # log: client sends its identifier, the server's rb, and ga mod p
+ 
             ct = crypto.encrypt(self._shared_key, pt) 
             self.client.send(ct)
             
@@ -250,15 +260,17 @@ class ChatClientServer(ChatClientBase):
         )
 
     def mutauth_step(self, reset=False):
-        if reset or self._shared_key is None:
-            # GUI should show something when shared key is None
+        if reset:
             self._mutau_state = self.MUTUAL_AUTH_STATES[-1]
             return
 
+        if self._shared_key is None:
+            raise NoSharedKey("Shared key is not setup yet. Can't proceed")
+
         if self._mutau_state == self.MUTUAL_AUTH_STATES[-1]:
-            # Receive client's nonce
+            # log: server receive client's ra
             self._Ra = self.server.recv()
-           
+            
             self._mutau_state = self.MUTUAL_AUTH_STATES[0]
         elif self._mutau_state == self.MUTUAL_AUTH_STATES[0]:
             # Send response: RB, E("Bob", RA, gb mod p, KAB)
@@ -268,25 +280,24 @@ class ChatClientServer(ChatClientBase):
             ct = crypto.encrypt(self._shared_key, pt)
             msg = self._Rb + ct
             self.client.send(msg)
-           
+            # log: server sends rb + its identifier, rb, and gb mod p
+
             self._mutau_state = self.MUTUAL_AUTH_STATES[1]
         elif self._mutau_state == self.MUTUAL_AUTH_STATES[1]:
             # Receive E("Alice", RB, ga mod p, KAB)
             ct = self.server.recv()
             pt = crypto.decrypt(self._shared_key, ct)
             identifier, rb, ga_mod_p = self.extract_auth_msg_parts(pt)
-            
-            # TODO: probably GUI should call this function with reset=True
-            # and display being attacked
+            # log: server receives client's identifier, rb, and ga mod p
+
             if rb != self._Rb:
                 raise BeingAttacked("Trudy is attacking")
-            # TODO: probably GUI should calls this function with reset=True 
-            # and display being attacked
             if identifier == self._identifier:
                 raise BeingAttacked("Trudy is doing replay attack")
 
             self._session_key = pow(long(ga_mod_p), self._secret_value, self._p)
-            
+            # log: server creates the session key
+
             self._mutau_state = self.MUTUAL_AUTH_STATES[2]
             self._session_key = crypto.derive_new_key(str(self._session_key))
         elif self._mutau_state == self.MUTUAL_AUTH_STATES[2]:
